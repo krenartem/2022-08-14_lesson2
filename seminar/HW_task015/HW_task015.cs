71ae0f3f7a5d87503e16288d635b3dfffffab566
krenartem <krenartem@yandex.ru>
2022-08-14 15:50:51 +0300

    add homework task 015

--- seminar/HW_task015/HW_task015.cs
+++ seminar/HW_task015/HW_task015.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите номер дня недели, от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber < 1 ^ dayNumber > 7)
+{
+    Console.WriteLine("Введенный номер вне диапазона от 1 до 7");
+}
+else
+{
+    if (dayNumber == 6 ^ dayNumber == 7)
+    {
+        Console.WriteLine($"УРА день {dayNumber} - ВЫХОДНОЙ!");
+    }
+    else
+    {
+        Console.WriteLine($"День недели {dayNumber} не является выходным");
+    }
+}
