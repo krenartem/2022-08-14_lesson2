e67d23582942fd850e41e63e139859c14342bce6
krenartem <krenartem@yandex.ru>
2022-08-14 13:57:46 +0300

    add homework task 013

--- seminar/HW_task013/HW_task013.cs
+++ seminar/HW_task013/HW_task013.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string numberString = Console.ReadLine();
+int length = numberString.Length;
+if (length < 3)
+{
+    Console.WriteLine($"В числе {numberString} третьей цифры нет");
+}
+else
+{
+    int number = Convert.ToInt32(numberString);
+    while (length > 3)
+    {
+        number = number / 10;
+        length = length - 1;
+    }
+    number = number % 10;
+    Console.WriteLine($"В числе {numberString} третья цифра {number}");
+}
